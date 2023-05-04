c4bd60dbce86662ff2f35523c2e3b285a9d0ec16
Brady Kelly <brady.kelly@lexisnexis.co.za>
2023-05-05 01:42:41 +0200

    Started LaunchThenDebug

---
+++ LaunchThenDebug/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+[System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
+extern static bool DebugActiveProcess(int dwProcessId);
+
+Console.WriteLine("Press any key to start the target process...");
+Console.ReadKey();
+
+// Start the target process
+
+var psi = new ProcessStartInfo("msiexec.exe", "C:\\Users\\kellyb\\Downloads\\vagrant_2.3.4_windows_amd64.msi /L*V \"vagrant-install.log\"");
+var targetProcess = Process.Start(psi);
+targetProcess?.WaitForExit();
